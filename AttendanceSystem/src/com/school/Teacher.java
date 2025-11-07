68350265e87200b22571b81c4839e007addf4681
saiabishekpriyan <saiabishek.priyan.s81@gmail.com>
2025-11-07 10:00:21 +0530

    feat: Apply SRP with RegistrationService and refined AttendanceService (Part 9)

--- AttendanceSystem/src/com/school/Teacher.java
+++ AttendanceSystem/src/com/school/Teacher.java
@@ -1,6 +1,6 @@
 package com.school;
 
-public class Teacher extends Person {
+public class Teacher extends Person implements Storable {
     private String subjectTaught;
 
     public Teacher(String name, String subjectTaught) {
@@ -8,9 +8,18 @@ public class Teacher extends Person {
         this.subjectTaught = subjectTaught;
     }
 
+    public String getSubjectTaught() {
+        return subjectTaught;
+    }
+
     @Override
     public void displayDetails() {
         super.displayDetails();
         System.out.println("Role: Teacher, Subject: " + subjectTaught);
+    }
+
+    @Override
+    public String toDataString() {
+        return getId() + "," + getName() + "," + subjectTaught;
     }
 }
